85330cb8f3b534eb3134ed91bfc217d3306199e9
Lolit Doucette <ldoucette@users.noreply.github.com>
2017-08-21 18:20:12 -0700

    Changed JSON format

--- panels.php
+++ panels.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 /*
- *	Author: Lolita Douccette
+*	Author: Lolita Douccette
 *	Version: 2016.09.23
 *
 */
@@ -10,36 +10,30 @@
 //error_reporting(E_ALL);
 
 // define the folder name of the comic images
-define("FOLDER", "_______" );   
-define ("COMIC_TITLE", "_______");
-define ("VOLUME_TITLE", "_______");
+define("FOLDER", "panels" );
+define ("COMIC_TITLE", "Alzyon");
+define ("VOLUME_TITLE", "The Beginning");
 
 	function __autoload($classname) {
    		$filename = 'class/' . $classname .".php";
    		include_once($filename);
    	}
-   	
+
    	$dLoader = new DirectoryLoader(FOLDER);
    	$paths = $dLoader->getCollection();
-   	
+
    	$iLoader = new ImageLoader(FOLDER);
-   	
+
    	$comic = new Comic(COMIC_TITLE, VOLUME_TITLE);
    	ComicLoader::loadComic($comic, $paths, $iLoader);
+
    	$chapters = $comic->getChapters();
 	$content = $comic->getContent();
-   	$page = (isset($_POST['page']))?$_POST['page']:0;
-   	$chapterNo = (isset($_POST['chapter']))?$_POST['chapter']:0;
-   	
+
+   	$chapterNo = (isset($_POST['chapterNo']))?$_POST['chapterNo']:0;
+
   	$chapter=$chapters[$chapterNo];
-  	$pages = $comic->getPages($chapter);
-  	$count = count($pages)-1;
-  	$status="current";
-  	if ($page<=0) $status="first";
-  	if ($page>=$count) $status="last";
- 	$current = $pages[$page];
-  	
-  	// all comic
-  	echo json_encode(array("chapters"=>$chapters, "numPanels"=>$count, "panel"=>$current, "status"=>$status));
-?>
- 
+    	$pages = $comic->getPages($chapter);
+
+  	echo json_encode(array("chapterNo"=>$chapterNo, "chapter"=>$chapter, "chapters"=>$chapters, "pages"=>$pages));
+ ?>
